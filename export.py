be83eefe4e5ef62345f7855fbd273fd455ec3df5
rravelli <remi.ravelli@gmail.com>
2023-11-13 17:56:17 +0100

    fix: export

--- export.py
+++ export.py
@@ -1,21 +1,23 @@
 from numpy import ndarray
 import pandas as pd
-import numpy as np
 import csv
 
 
 def export_to_csv(y_predict: ndarray, file_name: str):
+    """Export prediction to correct csv file"""
+
     df = pd.DataFrame(
-        np.array([np.arange(len(y_predict)) + 1, y_predict]).T,
-        columns=["", "p0q0"],
+        y_predict,
+        columns=["p0q0"],
     )
 
-    df[""] = df[""].astype("str")
-
+    # df[""] = df[""].astype("str")
+    df.index += 1
+    df.index = df.index.astype("str")
     df.to_csv(
         f"export/{file_name}.csv",
         sep=",",
-        index=False,
+        index=True,
         doublequote=True,
         quoting=csv.QUOTE_NONNUMERIC,
     )
