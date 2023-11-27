71e87a07e5135132f95399c72ce64c93d47d88cf
ManonC12 <manon-c.coutier@wanadoo.fr>
2023-11-27 16:42:08 +0100

    Fixes and merged_notebook

---
+++ 3_best_models_improvement/3_2_stack_regressor/export.py
@@ -0,0 +1,23 @@
+from numpy import ndarray
+import pandas as pd
+import csv
+
+
+def export_to_csv(y_predict: ndarray, file_name: str):
+    """Export prediction to correct csv file"""
+
+    df = pd.DataFrame(
+        y_predict,
+        columns=["p0q0"],
+    )
+
+    # df[""] = df[""].astype("str")
+    df.index += 1
+    df.index = df.index.astype("str")
+    df.to_csv(
+        f"../../../export/{file_name}.csv",
+        sep=",",
+        index=True,
+        doublequote=True,
+        quoting=csv.QUOTE_NONNUMERIC,
+    )
